ad8e66f505144fe95eff9238bff0906a5866fc61
ZR <zikriramdani.developer@gmail.com>
2021-06-19 04:32:06 +0700

    pagination api

--- api/controllers/productController.js
+++ api/controllers/productController.js
@@ -2,7 +2,7 @@ const models = require("../../models");
 
 // Pagination
 const getPagination = (page, size) => {
-    const limit = size ? +size : 3;
+    const limit = size ? +size : 6;
     const offset = page ? page * limit : 0;
   
     return { limit, offset };
